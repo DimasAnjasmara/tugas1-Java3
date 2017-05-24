b9ebbc47349336b0f26cbc9bec6c18d621d448f9
dimas anjasmara <dimas.anjasmara@gmail.com>
2017-05-24 16:17:51 +0700

    3.Mencari luas lingkaran

--- HitungLuas.java
+++ HitungLuas.java
@@ -3,6 +3,10 @@ class hitungluas {
 	public static int cariLuasPersegiPanjang(int panjang, int lebar) {
 		return panjang * lebar;	
 	}
+
+	public static float cariLuasLingkaran(int jari2) {
+		return (22/7) * jari2 * jari2;
+	}
 	
 	public static void main (String[] args) {
 
